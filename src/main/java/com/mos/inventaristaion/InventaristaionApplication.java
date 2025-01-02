e3d4885837c29d43910b63881469cb979ec45436
Jakub Maciocha <ciamcio@dellg15jm.home>
2025-01-02 19:41:00 +0100

    Inital commit

---
+++ src/main/java/com/mos/inventaristaion/InventaristaionApplication.java
@@ -0,0 +1,13 @@
+package com.mos.inventaristaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaristaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaristaionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mos/inventaristaion/InventaristaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mos.inventaristaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaristaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
